dc95222e8d7a8f8003de0999495dd77fc74df70a
TAIKI KONDO <cbu_hh@yahoo.co.jp>
2017-04-14 12:12:41 +0000

    hahaha_commit

--- db/migrate/20170220151316_create_blogs.rb
+++ db/migrate/20170220151316_create_blogs.rb
@@ -2,8 +2,9 @@ class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
       t.string :title
+      t.string :name
       t.text :content
-
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170414094421) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
+    t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

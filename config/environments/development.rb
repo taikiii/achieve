597fafba9db76069e7b60cc22d6e0747cfe73c3f
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-03 21:43:33 +0900

    yusyuc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,15 +42,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings =
-  {
-  user_name: ENV['app65717617@heroku.com'],
-  password: ENV['jfb20oxw5473'],
-  domain: "heroku.com",
-  address: "smtp.sendgrid.net",
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-  }
 end

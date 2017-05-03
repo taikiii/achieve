60275ee21eaa71affa5c6fdfe9a09cde690190b9
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-03 19:58:18 +0900

    tyatyatya

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -15,7 +15,7 @@ class NoticeMailer < ApplicationMailer
   def sendmail_contact(contact)
     @contact = contact
 
-    mail to: "@contact.email",
+    mail to: @contact.email,
          subject: '【Achieve】お問い合わせが完了しました'
   end
 

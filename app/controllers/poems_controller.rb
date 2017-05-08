a7cfef4c8c6fc60fd8bebaa9e03a9fe7c51fe402
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-08 21:52:20 +0900

    hrvenne

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,4 +2,7 @@ class PoemsController < ApplicationController
   def index
     @poems = Poem.all
   end
+
+  def show
+    @poems = Poem.all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  resources :poems, only: [:index]
-  
+  resources :poems, only: [:index, :show]
+
   root 'top#index'
 
   get 'blogs' => 'blogs#index'

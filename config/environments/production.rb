08f726b3c54e014dc5bf1ea22a6a3bb76fe1403c
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-09 21:48:43 +0900

    kkkkkk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'thawing-fortress-52549' }
+  config.action_mailer.default_url_options = { host: 'thawing-fortress-52549.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {

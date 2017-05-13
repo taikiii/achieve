061d868a59ea9013dee5f42560699ff950e1faf7
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-13 19:49:09 +0900

    kokkkokokok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
+  devise_for :users, controllers: {
+      registrations: "users/registrations",
+      omniauth_callbacks: "users/omniauth_callbacks"
+      }
+
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy, :subject] do
     collection do
       post :confirm
@@ -76,8 +81,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    omniauth_callbacks: "users/omniauth_callbacks"
-  }
 end

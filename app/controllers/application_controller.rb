9808a6c6919c475dedb63bf41b4bcbffcb895a4f
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-06-19 22:45:14 +0900

    jjjj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :current_notifications, if: :signed_in?
 
   PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
     devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+   end
+
+   def current_notifications
+    @notifications_count = Notification.where(user_id: current_user.id).where(read: false).count
    end
 end

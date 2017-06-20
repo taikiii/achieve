f43b6a29db576f9539b6a3964d796efedea57c65
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-06-20 23:22:00 +0900

    hhhh

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,12 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+
+  def read_or(read)
+    if read == false
+      "未読"
+    else
+      "既読"
+    end
+  end
 end
